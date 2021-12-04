562b6f2ffe820ad35991868de6225cc4cc180c4c
Bryan Hernandez <brayannhc07@gmail.com>
2021-12-04 09:14:49 -0600

    Seccion 14: Respaldo avance

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -8,7 +8,7 @@ import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, RouterModule, FormsModule],
+  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule],
   exports: [LoginComponent, RegisterComponent],
 })
-export class AuthModule {}
+export class AuthModule { }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,12 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent implements OnInit {
-  constructor() {}
+export class RegisterComponent {
+  public registerForm = this.fb.group({
+    nombre: ["Bryan", Validators.required],
+    email: ["test100@gmail.com", Validators.required],
+    password: ["123456", Validators.required],
+    password2: ["123456", Validators.required],
+    terminos: [false, Validators.required]
+  });
 
-  ngOnInit(): void {}
+  constructor(private fb: FormBuilder) {
+
+  }
+
 }
