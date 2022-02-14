d530b2affbcad6cc1e7851b81cd571a198085442
Brayan Hernandez <brayannhc07@gmail.com>
2022-02-14 09:39:58 -0600

    Seccion 14: Respaldo avance (Google sign in

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,8 @@
+import { FormBuilder } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
+declare const gapi: any;
 
 @Component({
   selector: 'app-login',
@@ -7,11 +10,61 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor(private router: Router) {}
 
-  ngOnInit(): void {}
+  public auth2: any;
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.renderButton();
+  }
 
   login(): void {
     this.router.navigateByUrl('/');
   }
+  onSuccess(googleUser: any) {
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log(id_token);
+  }
+
+  onFailure(error: any) {
+    console.log(error);
+  }
+
+  renderButton() {
+    gapi.signin2.render('my-signin2', {
+      scope: 'profile email',
+      width: 240,
+      height: 50,
+      longtitle: true,
+      theme: 'dark',
+    });
+
+    this.startApp();
+  }
+
+  startApp() {
+    gapi.load('auth2', () => {
+      // Retrieve the singleton for the GoogleAuth library and set up the client.
+      this.auth2 = gapi.auth2.init({
+        client_id: '711704106982-qgabo3kkv2n6kuhi6lmps3lb6go0hp3q.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        // Request scopes in addition to 'profile' and 'email'
+        //scope: 'additional_scope'
+      });
+      this.attachSignin(document.getElementById('customBtn'));
+    });
+  };
+
+  attachSignin(element: any) {
+    this.auth2.attachClickHandler(element, {},
+      (googleUser: any) => {
+        const id_token = googleUser.getAuthResponse().id_token;
+        console.log(id_token);
+      }, (error: any) => {
+        alert(JSON.stringify(error, undefined, 2));
+      });
+  }
+
 }
