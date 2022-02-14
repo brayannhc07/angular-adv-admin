7db48b504df97ea921c17d821bb2d022568920d3
Brayan Hernández <brayannhc07@gmail.com>
2022-02-14 09:37:23 -0600

    Seccion 14: Respaldo avance (Forms)

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,17 +1,40 @@
+import { UsuarioService } from './../../services/usuario.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor(private router: Router) {}
 
-  ngOnInit(): void {}
+  public loginForm = this.fb.group({
+    email: [localStorage.getItem("email") || "", [Validators.required, Validators.email]],
+    password: ["", Validators.required],
+    remember: [false]
+  });
+
+
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private usuarioService: UsuarioService) { }
+
+  ngOnInit(): void { }
 
   login(): void {
-    this.router.navigateByUrl('/');
+    this.usuarioService.login(this.loginForm.value)
+      .subscribe(resp => {
+        if (this.loginForm.get("remember")?.value) {
+          localStorage.setItem("email", this.loginForm.get("email")?.value);
+        } else {
+          localStorage.removeItem("email");
+        }
+      }, (err) => {
+        Swal.fire("Error", err.error.msg, "error");
+      });
+    // this.router.navigateByUrl('/');
   }
 }
